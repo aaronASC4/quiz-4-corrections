96ab5fa9b58c134d302ca8fcd4da06de3f31bd1a
aaronASC4 <aarongraves07@gmail.com>
2019-01-21 00:02:18 -0500

    Create quizVowels.java

---
+++ quizVowels.java
@@ -0,0 +1,22 @@
+public class quizVowels {
+	
+public int vowels(String original) {
+if (original == null || original == "") {
+			return null;
+		}
+		
+		for(int i = 0; i < original.length(); i++) {
+			if (original.charAt(i) == 'a' || original.charAt(i) == 'e' || original.charAt(i) == 'i'
+                    || original.charAt(i) == 'o' || original.charAt(i) == 'u')
+            {
+                count++;
+				
+			}
+		} 
+		
+	
+}
+		
+	}
+	
+	
